db4e462a3e483dbcfe39ff4611fc0bcbb36477e0
Tihh Gonçalves <tihh.gon@gmail.com>
2016-12-15 15:43:06 -0200

    Agora mostra resolução da tela por 5 segundos

--- bootstrap-device-debug.js
+++ bootstrap-device-debug.js
@@ -4,5 +4,15 @@ $(document).ready(function(e) {
 	$('body').append('<div id="deviceDebugMD" class="deviceDebug">.md</div>');
 	$('body').append('<div id="deviceDebugSM" class="deviceDebug">.sm</div>');
 	$('body').append('<div id="deviceDebugXS" class="deviceDebug">.xs</div>');
+	$('body').append('<div id="deviceDebugResolution"">0x0</div>');
+
+
+	$('#deviceDebugResolution').text( $(window).width() + ' x ' + $(window).height() );
+
+	setTimeout(function(){
+
+		$('#deviceDebugResolution').fadeOut();
+
+	}, 5000);
 	
 });
