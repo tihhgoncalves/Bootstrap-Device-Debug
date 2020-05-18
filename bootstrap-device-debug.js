04441090d4a410f9b5c61c7f554b849ca2c3ad0c
Tihh Gonçalves <tihh.gon@gmail.com>
2020-05-18 14:10:27 -0300

    Update bootstrap-device-debug.js

--- bootstrap-device-debug.js
+++ bootstrap-device-debug.js
@@ -1,6 +1,7 @@
 $(document).ready(function(e) {
     
-	$('body').append('<div id="deviceDebugLG" class="deviceDebug">.lg</div>');
+	$('body').append('<div id="deviceDebugXL" class="deviceDebug">.xl</div>');
+        $('body').append('<div id="deviceDebugLG" class="deviceDebug">.lg</div>');
 	$('body').append('<div id="deviceDebugMD" class="deviceDebug">.md</div>');
 	$('body').append('<div id="deviceDebugSM" class="deviceDebug">.sm</div>');
 	$('body').append('<div id="deviceDebugXS" class="deviceDebug">.xs</div>');
